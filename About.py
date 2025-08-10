f289efbf90d282101e10e539f24ada798f55f3a0
Shiv Sunder Behera <Shivsunder3109@gmail.com>
2025-08-10 22:28:30 +0530

    Update About.py
    some changes in about.py by adding my linkedin address

--- About.py
+++ About.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 def app():
-    st.subheader('Wellcome to your AI driven Personalised Travel Itinerary Generator')
+    st.subheader('Welcome to your AI driven Personalised Travel Itinerary Generator')
     st.subheader('Travel and live the life fullest')
-    st.markdown('Created by: [Rajesh Kumar Jena](https://www.linkedin.com/in/rajesh-kumar-jena-793257212/)')
-    st.markdown('Contact via mail: [getrajeshkumarjena@gmail.com]')
+    st.markdown('Created by: [Shiv Sunder Behera](https://www.linkedin.com/in/shivsunderbehera31/)')
+    st.markdown('Contact via mail: [shivsunder3109@gmail.com]')
     
